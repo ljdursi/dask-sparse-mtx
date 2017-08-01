b29df2392fdbb7765dcaa8c2cd9ac5d39de669dd
Jonathan Dursi <jonathan@dursi.ca>
2017-08-01 08:08:06 -0400

    MIT License

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 with open('README.rst') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open('LICENSE.md') as f:
     license = f.read()
 
 setup(
